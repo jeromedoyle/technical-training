2e2dafe91aa9f59714392f8e1def7f89cf3ef3db
Jerome Doyle <jeromedoylejr@gmail.com>
2022-10-10 10:41:28 +0200

    make name and expected price required

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Estate",  # The name that will appear in the App list
-    "version": "16.2",  # Version
+    "version": "16.3",  # Version
     "application": True,  # This line says the module is an App, and not a module
     "depends": ["base"],  # dependencies
     "data": [],
