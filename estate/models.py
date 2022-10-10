8f858f3f1c081fb7b772ff4b04a679836d4a4e02
Jerome Doyle <jeromedoylejr@gmail.com>
2022-10-10 13:34:56 +0200

    Update fields names

--- estate/models.py
+++ estate/models.py
@@ -6,16 +6,16 @@ class EstateProperty(models.Model):
     _name = "estate.property"
     _description = "Estate Property Model"
 
-    name = fields.Char(required=True)
+    name = fields.Char(string="Title", required=True)
     description = fields.Text()
     postcode = fields.Char()
     date_availability = fields.Date(
-        copy=False, default=date.today() + timedelta(days=90)
+        string="Available From", copy=False, default=date.today() + timedelta(days=90)
     )
     expected_price = fields.Float(required=True)
     selling_price = fields.Float(readonly=True, copy=False)
     bedrooms = fields.Integer(default=2)
-    living_area = fields.Integer()
+    living_area = fields.Integer(string="Living Area (sqm)")
     facades = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
