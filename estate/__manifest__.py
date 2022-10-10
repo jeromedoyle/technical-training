aab63e17f22f79c2dec8fdbab7bcb3c96298992f
Jerome Doyle <jeromedoylejr@gmail.com>
2022-10-10 11:35:32 +0200

    add menu items

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -3,7 +3,7 @@
     "version": "16.3",  # Version
     "application": True,  # This line says the module is an App, and not a module
     "depends": ["base"],  # dependencies
-    "data": ["security/ir.model.access.csv"],
+    "data": ["security/ir.model.access.csv", "views/estate_property_views.xml"],
     "installable": True,
     "license": "LGPL-3",
 }
