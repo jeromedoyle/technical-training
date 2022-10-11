76c352dc72f71ec412a28e09dc1ff0b336ae7bcb
Jerome Doyle <jeromedoylejr@gmail.com>
2022-10-11 13:36:19 +0200

    add back tags

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Estate",  # The name that will appear in the App list
-    "version": "16.7",  # Version
+    "version": "16.8",  # Version
     "application": True,  # This line says the module is an App, and not a module
     "depends": ["base"],  # dependencies
     "data": ["security/ir.model.access.csv", "views/estate_property_views.xml"],
