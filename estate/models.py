50088885a368b75b02e2a90566ebe7fc9c43dbac
Jerome Doyle <jeromedoylejr@gmail.com>
2022-10-11 13:58:10 +0200

    change view names

--- estate/models.py
+++ estate/models.py
@@ -44,7 +44,7 @@ class EstateProperty(models.Model):
         default="new",
     )
     property_type_id = fields.Many2one("estate.property.type", string="Property Type")
-    buyer = fields.Many2one("res.partner", string="Buyer")
+    buyer = fields.Many2one("res.partner", string="Buyer", copy=False)
     seller = fields.Many2one(
         "res.users", string="Salesperson", default=lambda self: self.env.user
     )
