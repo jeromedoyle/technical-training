a6b9d10ba2b07bba120843f8747b543524bfad2f
Jerome Doyle <jeromedoylejr@gmail.com>
2022-10-11 13:42:00 +0200

    fix object name

--- estate/models.py
+++ estate/models.py
@@ -43,7 +43,7 @@ class EstateProperty(models.Model):
         help="The current state of the listing",
         default="new",
     )
-    property_type_id = fields.Many2one("estate.property_type", string="Property Type")
+    property_type_id = fields.Many2one("estate.property.type", string="Property Type")
     buyer = fields.Many2one("res.partner", string="Buyer")
     seller = fields.Many2one(
         "res.users", string="Salesperson", default=lambda self: self.env.user
